2bbfc0e366578f0bfa8c4407d4de359fa9b4ad20
Falahh321 <104072379+Falahh321@users.noreply.github.com>
2026-06-27 22:45:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -114,3 +114,30 @@ if(hour < 12){
 }
 
 console.log(message);
+/* ===== 3D Card ===== */
+
+const card =
+document.querySelector(".card") ||
+document.querySelector(".container") ||
+document.querySelector(".main-card");
+
+if(card){
+
+document.addEventListener("mousemove",(e)=>{
+
+const x=(window.innerWidth/2-e.clientX)/25;
+
+const y=(window.innerHeight/2-e.clientY)/25;
+
+card.style.transform=
+`rotateY(${-x}deg) rotateX(${y}deg)`;
+
+});
+
+document.addEventListener("mouseleave",()=>{
+
+card.style.transform="rotateY(0deg) rotateX(0deg)";
+
+});
+
+}

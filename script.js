18390d85bb6472f7e1c38f9594006b9b103479bb
Falahh321 <104072379+Falahh321@users.noreply.github.com>
2026-06-27 17:57:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-document.querySelectorAll(".btn").forEach(btn=>{
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("mouseenter", () => {
+        btn.style.transform = "scale(1.03)";
+    });
 
-btn.addEventListener("mousemove",e=>{
-
-const x=e.offsetX;
-const y=e.offsetY;
-
-btn.style.background=`radial-gradient(circle at ${x}px ${y}px,#d400ff,#6a00ff,#3a0066)`;
-
+    btn.addEventListener("mouseleave", () => {
+        btn.style.transform = "scale(1)";
+    });
 });
 
-btn.addEventListener("mouseleave",()=>{
+const profile = document.querySelector(".profile-img");
 
-btn.style.background="linear-gradient(90deg,#7a00ff,#c000ff,#7a00ff)";
-
+profile.addEventListener("mousemove", () => {
+    profile.style.transform = "scale(1.05)";
 });
 
+profile.addEventListener("mouseleave", () => {
+    profile.style.transform = "scale(1)";
+});
+
+window.addEventListener("load", () => {
+    document.querySelector(".profile-box").style.opacity = "1";
+    document.querySelector(".profile-box").style.transform = "translateY(0)";
 });

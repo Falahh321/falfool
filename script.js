16d863624851faee8b326c90895291c8bcd11c5f
Falahh321 <104072379+Falahh321@users.noreply.github.com>
2026-06-29 18:22:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+function updateDateTime() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString("ar-IQ", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+
+    const date = now.toLocaleDateString("ar-IQ", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    document.getElementById("time").textContent = time;
+    document.getElementById("date").textContent = date;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
